7db47384fb1c51f4fa5908a20d73d8af83e34267
Lungelo Ngcemu <lungelofarm@gmail.omm>
2024-10-29 22:31:13 +0200

    close

--- screens/HomeScreens/about.tsx
+++ screens/HomeScreens/about.tsx
@@ -6,9 +6,20 @@ export default function About() {
     return (
 
         <SafeAreaView>
-            <View style = {styles.page}>
-                <Text>About</Text>
-             
+            <View style={styles.page}>
+                <Text style={styles.head}>Founder Precious Radebe</Text>
+                <Text style={styles.text}>
+
+                    The SME, founded by Precious Radebe, offers skills training for domestic workers and gardeners, 
+                    inspired by her desire to help others in her community who, like her parents, 
+                    didn’t have opportunities to pursue formal education or skill development. {"\n\n"}
+
+
+                    Her training equips domestic workers and gardeners with valuable skills,
+                     making them more competitive for higher-paying jobs and enabling them to start their own small businesses. 
+                     Many employers also appreciate having more skilled workers who can offer enhanced services at home.
+
+                </Text>
             </View>
         </SafeAreaView>
     );
@@ -16,9 +27,25 @@ export default function About() {
 
 const styles = StyleSheet.create({
     page: {
-     backgroundColor: 'orange',
-      height: '80%'
+        justifyContent: "center",
+        height: '75%',
+        display: "flex",
+        marginTop: 5
     },
-  
-  });
-  
+
+    head: {
+        fontWeight: "bold",
+        fontSize: 20,
+        textAlign: "center",
+
+    },
+
+
+    text: {
+        fontSize: 18,
+        marginTop: 5,
+        padding : 10
+    }
+
+});
+
--- screens/HomeScreens/locations.tsx
+++ screens/HomeScreens/locations.tsx
@@ -1,24 +1,84 @@
-import * as React from 'react'
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import MapView, { UrlTile, Marker } from 'react-native-maps';
 
+export default function App() {
 
-export default function Locations() {
-    return (
+    const markers = [
+        {
+          id: 1,
+          title: 'Downtown Learning Center',
+          description: 'A hub for technology and business courses in the heart of the city.',
+          latitude: 37.7749,
+          longitude: -122.4194,
+        },
+        {
+          id: 2,
+          title: 'Nations',
+          description: 'Short and Long courses',
+          latitude: 37.7849,
+          longitude: -122.4094,
+        },
+        {
+          id: 3,
+          title: 'Nations',
+          description: 'Short and Long courses',
+          latitude: 37.7949,
+          longitude: -122.4294,
+        },
+        {
+          id: 4,
+          title: 'Nations',
+          description: 'Short and Long courses',
+          latitude: 37.7649,
+          longitude: -122.4394,
+        },
+        {
+          id: 5,
+          title: 'Nations',
+          description: 'Short and Long courses',
+          latitude: 37.7549,
+          longitude: -122.4494,
+        },
+      ];
 
-        <SafeAreaView>
-            <View style = {styles.page}>
-                <Text>Location</Text>
-             
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.7749,
+          longitude: -122.4194,
+          latitudeDelta: 0.044,
+          longitudeDelta: 0.05,
+        }}
+      >
+        <UrlTile
+          urlTemplate="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+          maximumZ={19}
+          flipY={false}
+        />
+          {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    page: {
-     backgroundColor: 'yellow',
-      height: '80%'
-    },
-  
-  });
-  
+  container: {
+    margin : 20,
+   
+    
+  },
+  map: {
+    width: '100%',
+    height: '90%',
+  },
+});
